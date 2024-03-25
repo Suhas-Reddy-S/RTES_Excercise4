41bea96a7420ac319e4626e9ea395cb874b6a9cf
Suhas-Reddy-S <suhasreddysedu@gmail.com>
2024-03-25 10:21:26 -0600

    added logging

--- Ex4_Suhas_and_John/computer_vision_cv4_tested/Ex4_Q5/Q5a.cpp
+++ Ex4_Suhas_and_John/computer_vision_cv4_tested/Ex4_Q5/Q5a.cpp
@@ -1,18 +1,86 @@
 #include <iostream>
 #include <pthread.h>
 #include <opencv2/opencv.hpp>
+#include <semaphore.h>
 
 using namespace cv;
 using namespace std;
 
+void end_delay_test(void);
+
+sem_t logSem, stopSem;
+struct timespec startTime, endTime;
+bool stop_logging = false;
+
 #define ESCAPE_KEY 27
 #define FRAME_DELAY 33  // Frame delay in milliseconds (about 30 frames per second)
+#define NSEC_PER_SEC (1000000000)
+#define DELAY_TICKS (1)
+#define ERROR (-1)
+#define OK (0)
 
 pthread_mutex_t cameraMutex; // Mutex for camera access
 
 struct ThreadData {
     VideoCapture* cam;
 };
+
+int delta_t(struct timespec *stop, struct timespec *start, struct timespec *delta_t)
+{
+  int dt_sec=stop->tv_sec - start->tv_sec;
+  int dt_nsec=stop->tv_nsec - start->tv_nsec;
+
+  if(dt_sec >= 0)
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+  }
+  else
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+  }
+
+  return(OK);
+}
+
+void* LoggingThread(void* threadp) {
+    struct timespec ts;
+    while (true) {
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_sec += 1; // Wait for up to 1 second
+        if (sem_timedwait(&stopSem, &ts) == -1) {
+            if (errno != ETIMEDOUT) {
+                break;
+            }
+        } else {
+        
+            break;
+        }
+        if (sem_trywait(&logSem) == 0) {
+            struct timespec elapsedTime;
+            delta_t(&endTime, &startTime, &elapsedTime);
+            cout << "Thread execution time: " << elapsedTime.tv_sec << "s " << elapsedTime.tv_nsec << "ns" << endl;
+        }
+    }
+    return nullptr;
+}
 
 void* CannyThread(void* arg) {
     ThreadData* data = static_cast<ThreadData*>(arg);
@@ -26,6 +94,7 @@ void* CannyThread(void* arg) {
 
     while(true) {
         pthread_mutex_lock(&cameraMutex);
+        clock_gettime(CLOCK_REALTIME, &startTime);
         bool success = data->cam->read(frame);
         pthread_mutex_unlock(&cameraMutex);
 
@@ -40,9 +109,11 @@ void* CannyThread(void* arg) {
         dst = Scalar::all(0);
         frame.copyTo(dst, detected_edges);
         imshow("Edge Map", dst);
-
+        clock_gettime(CLOCK_REALTIME, &endTime);
+        sem_post(&logSem);
         char c = (char)waitKey(FRAME_DELAY);
         if (c == ESCAPE_KEY) {
+            stop_logging = true;
             break;
         }
     }
@@ -57,6 +128,7 @@ void* HoughLinesThread(void* arg) {
 
     while (true) {
         pthread_mutex_lock(&cameraMutex);
+        clock_gettime(CLOCK_REALTIME, &startTime);
         bool success = data->cam->read(frame);
         pthread_mutex_unlock(&cameraMutex);
 
@@ -87,9 +159,11 @@ void* HoughLinesThread(void* arg) {
 
         // Show results
         imshow("Detected Lines", cdstP);
-
+        clock_gettime(CLOCK_REALTIME, &endTime);
+        sem_post(&logSem);
         char c = (char)waitKey(1);
         if (c == ESCAPE_KEY) {
+            stop_logging = true;
             break;
         }
     }
@@ -104,6 +178,7 @@ void* PyrUpDownThread(void* arg) {
 
     while (true) {
         pthread_mutex_lock(&cameraMutex);
+        clock_gettime(CLOCK_REALTIME, &startTime);
         bool success = data->cam->read(frame);
         pthread_mutex_unlock(&cameraMutex);
 
@@ -130,6 +205,8 @@ void* PyrUpDownThread(void* arg) {
         }
 
         imshow("Pyramids Demo", frame); // Redisplay the frame after zoom operation
+        clock_gettime(CLOCK_REALTIME, &endTime);
+        sem_post(&logSem);
         waitKey(delayMs); // Add delay to observe the zoom effect
     }
     return nullptr;
@@ -149,6 +226,7 @@ int main(int argc, char *argv[]) {
 
     // Initialize the camera mutex
     pthread_mutex_init(&cameraMutex, NULL);
+    sem_init(&logSem, 0, 0);
 
     int width = parser.get<int>("res_w");
     int height = parser.get<int>("res_h");
@@ -164,6 +242,9 @@ int main(int argc, char *argv[]) {
 
     ThreadData data;
     data.cam = &cam0;
+    
+    pthread_t logThread;
+    pthread_create(&logThread, NULL, LoggingThread, NULL);
 
     pthread_t thread;
     int rc;
@@ -185,9 +266,11 @@ int main(int argc, char *argv[]) {
     }
 
     pthread_join(thread, NULL); // Wait for the thread to finish
+    sem_post(&stopSem);
+    pthread_join(logThread, NULL);
 
     pthread_mutex_destroy(&cameraMutex);
+    sem_destroy(&logSem);
 
     return 0;
 }
-
